a02a6e39a89636e8c2c91a9a8ed605a24c6cf4bf
farzad <farzad@postr.co.nz>
2017-05-03 16:55:34 +1200

    FARTest1 - Connection Stuff: Tidy up, make modular  - 3/5/2017

--- src/main/java/POJOs/ConnectionSVG.java
+++ src/main/java/POJOs/ConnectionSVG.java
@@ -4,5 +4,99 @@ package POJOs;
  * Created by FARzad - VOLCANO on 5/3/2017.
  */
 public class ConnectionSVG {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+    private int width;
+    private int[] dashArray;
+    private String color;
+    private String type;
+    private String name;
 
+    public int getX1() {
+        return x1;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int[] getDashArray() {
+        return dashArray;
+    }
+
+    public void setDashArray(int[] dashArray) {
+        this.dashArray = dashArray;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "ConnectionSVG{" +
+                "x1=" + x1 +
+                ", x2=" + x2 +
+                ", y1=" + y1 +
+                ", y2=" + y2 +
+                ", width=" + width +
+                ", color='" + color + '\'' +
+                ", type='" + type + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/Farzad/utils/ImageUtils/ConnectionTools.java
+++ src/main/java/com/Farzad/utils/ImageUtils/ConnectionTools.java
@@ -1,5 +1,6 @@
 package com.Farzad.utils.ImageUtils;
 
+import POJOs.ConnectionSVG;
 import POJOs.SVGSingleShape;
 import com.Farzad.Enums.ConnectionsEnum;
 
@@ -11,23 +12,23 @@ class ConnectionTools {
 //        System.out.println(source !=null?source.getName() +" target id : "+target.getId()+" target name : "+target.getName():"nuuul");
         if (source != null && source.getConnectionsType() != null) {
 //            System.out.println(source.getConnectionsType()+"--**--"+source.getName()+" target id : "+target.getId()+" target name : "+target.getName());
-            if(ConnectionsEnum.ACCESSES.equalsName(source.getConnectionsType().toLowerCase())) {
-                return lineSVGCode(source, target,ConnectionsEnum.ACCESSES);
-            }else if(ConnectionsEnum.ASSIGNED.equalsName(source.getConnectionsType().toLowerCase())){
-                return lineSVGCode(source, target,ConnectionsEnum.ASSIGNED);
-            }else if(ConnectionsEnum.AGGREGATION.equalsName(source.getConnectionsType().toLowerCase())){
-                return lineSVGCode(source, target,ConnectionsEnum.AGGREGATION);
-            }else if(ConnectionsEnum.ASSOCIATION.equalsName(source.getConnectionsType().toLowerCase())){
-                return lineSVGCode(source, target,ConnectionsEnum.ASSOCIATION);
-            }else if(ConnectionsEnum.USES.equalsName(source.getConnectionsType().toLowerCase())){
-                return lineSVGCode(source, target,ConnectionsEnum.USES);
-            }else if(ConnectionsEnum.USED_BY.equalsName(source.getConnectionsType().toLowerCase())){
-                return lineSVGCode(source, target,ConnectionsEnum.USED_BY);
-            }else if(ConnectionsEnum.REALISES.equalsName(source.getConnectionsType().toLowerCase())){
-                return lineSVGCode(source, target,ConnectionsEnum.REALISES);
-            }else if(ConnectionsEnum.READ_AND_WRITE.equalsName(source.getConnectionsType().toLowerCase())){
-                return lineSVGCode(source, target,ConnectionsEnum.READ_AND_WRITE);
-            }else
+            if (ConnectionsEnum.ACCESSES.equalsName(source.getConnectionsType().toLowerCase())) {
+                return lineSVGCode(source, target, ConnectionsEnum.ACCESSES);
+            } else if (ConnectionsEnum.ASSIGNED.equalsName(source.getConnectionsType().toLowerCase())) {
+                return lineSVGCode(source, target, ConnectionsEnum.ASSIGNED);
+            } else if (ConnectionsEnum.AGGREGATION.equalsName(source.getConnectionsType().toLowerCase())) {
+                return lineSVGCode(source, target, ConnectionsEnum.AGGREGATION);
+            } else if (ConnectionsEnum.ASSOCIATION.equalsName(source.getConnectionsType().toLowerCase())) {
+                return lineSVGCode(source, target, ConnectionsEnum.ASSOCIATION);
+            } else if (ConnectionsEnum.USES.equalsName(source.getConnectionsType().toLowerCase())) {
+                return lineSVGCode(source, target, ConnectionsEnum.USES);
+            } else if (ConnectionsEnum.USED_BY.equalsName(source.getConnectionsType().toLowerCase())) {
+                return lineSVGCode(source, target, ConnectionsEnum.USED_BY);
+            } else if (ConnectionsEnum.REALISES.equalsName(source.getConnectionsType().toLowerCase())) {
+                return lineSVGCode(source, target, ConnectionsEnum.REALISES);
+            } else if (ConnectionsEnum.READ_AND_WRITE.equalsName(source.getConnectionsType().toLowerCase())) {
+                return lineSVGCode(source, target, ConnectionsEnum.READ_AND_WRITE);
+            } else
                 return null;
 //            int x1 = source.getX() + source.getWidth();
 //            int x2 = target.getX();
@@ -44,82 +45,110 @@ class ConnectionTools {
 //                    ;
 
         } else
-            return source !=null?source.getName():null;
+            return source != null ? source.getName() : null;
 
     }
-     private static String lineSVGCode(SVGSingleShape source, SVGSingleShape target, ConnectionsEnum connectionEnum) {
-        int x1 = source.getX() + source.getWidth();
-        int x2 = target.getX();
-        int y1 = source.getY();
-        int y2 = target.getY();
-        int lineWidth = source.getStrokeWidth();
+
+    private static String lineSVGCode(SVGSingleShape source, SVGSingleShape target, ConnectionsEnum connectionEnum) {
+        ConnectionSVG conSVG = new ConnectionSVG();
+        conSVG.setX1(source.getX() + source.getWidth()/2);
+        conSVG.setX2(target.getX()+target.getWidth()/2);
+        conSVG.setY1(source.getY());
+        conSVG.setY2(target.getY());
+        conSVG.setWidth(source.getStrokeWidth());
+        conSVG.setColor("black");
+//        int lineWidth = source.getStrokeWidth();
         switch (connectionEnum) {
             case ACCESSES:
-                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
-                        "      <line stroke-dasharray=\"2, 2\" fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
-                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
-                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
+                return (makeLine(conSVG) + putText(conSVG, target));
+//              return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
+//                        "      <line stroke-dasharray=\"2, 2\" fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
+//                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
+//                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
             case USED_BY:
-                return
-// " <polygon points=\"" + x1 + "," + y1 + " " + (x1+20) + "," + (y1-10) + " " + (x1+40) + "," + y1 + " " + (x1+50) + "," + (y1+10) + "\"/>" +
-                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
-                                "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
-                                "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />"+
-                                "      <text x=\"" + (x1+x2)/2 + "\" xml:space=\"preserve\" y=\"" + (y1+y2)/2 + "\" clip-path=\"url(#clipPath20)\" stroke=\"none\"\n" +
-                                "      font-family=\"" + source.getFont() + "\" fill=\"#000000\"  >" + target.getConnectionsType() + "</text>\n"
-                        ;
+                return (makeLine(conSVG) + putText(conSVG, target));
+//              " <polygon points=\"" + x1 + "," + y1 + " " + (x1+20) + "," + (y1-10) + " " + (x1+40) + "," + y1 + " " + (x1+50) + "," + (y1+10) + "\"/>" +
+//                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
+//                                "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
+//                                "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />"+
+//                                "      <text x=\"" + (x1+x2)/2 + "\" xml:space=\"preserve\" y=\"" + (y1+y2)/2 + "\" clip-path=\"url(#clipPath20)\" stroke=\"none\"\n" +
+//                                "      font-family=\"" + source.getFont() + "\" fill=\"#000000\"  >" + target.getConnectionsType() + "</text>\n"
+//                        ;
             case ASSIGNED:
-                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
-                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
-                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
-                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
+                return (makeLineWithDoubleOrb(makeLine(conSVG),conSVG) + putText(conSVG, target));
+//                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
+//                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
+//                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
+//                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
             case REALISES:
-                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
-                        "      <line stroke-dasharray=\"5, 5\" fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
-                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
-                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
+                conSVG.setDashArray(new int[]{5,5});
+                return (makeLine(conSVG) + putText(conSVG, target));
+//                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
+//                        "      <line stroke-dasharray=\"5, 5\" fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
+//                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
+//                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
             case USES:
-                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
-                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
-                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
-                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
+                return (makeLine(conSVG) + putText(conSVG, target));
+//                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
+//                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
+//                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
+//                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
             case AGGREGATION:
-                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
-                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
-                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
-                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
+                return (makeLine(conSVG) + putText(conSVG, target));
+//                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
+//                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
+//                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
+//                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
             case ASSOCIATION:
-                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
-                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
-                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
-                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
+                return (makeLine(conSVG) + putText(conSVG, target));
+//                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
+//                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
+//                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
+//                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
             case READ_AND_WRITE:
-                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
-                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
-                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
-                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
+                return (makeLine(conSVG) + putText(conSVG, target));
+//                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
+//                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
+//                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
+//                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
             default:
-                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
-                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
-                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
-                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
+                return (makeLine(conSVG) + putText(conSVG, target));
+//                return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
+//                        "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
+//                        "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
+//                        "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
         }
     }
-    private static String makeLine(SVGSingleShape source, SVGSingleShape target) {
-        int x1 = source.getX() + source.getWidth();
-        int x2 = target.getX();
-        int y1 = source.getY();
-        int y2 = target.getY();
-        int lineWidth = source.getStrokeWidth();
-        return  " <circle cx=\"" + x1 + "\" cy=\"" + y1 + "\" r=\"3\" fill=\"#000000\" />" +
-                "      <line fill=\"black\" x1=\"" + x1 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" y2=\"" + y2 + "\" " +
-                "stroke=\"#000000\" stroke-width=\"" + lineWidth + "\"/>\n" +
-                "<circle cx=\"" + x2 + "\" cy=\"" + y2 + "\" r=\"3\" fill=\"#000000\" />";
+
+    private static String makeLine(ConnectionSVG svg) {
+        int dashWidth = 0, dashGap = 0;
+        if (svg.getDashArray() != null && svg.getDashArray().length > 1) {
+            dashWidth = svg.getDashArray()[0];
+            dashGap = svg.getDashArray()[1];
+        }
+        return
+                "      <line fill=\"black\" stroke-dasharray=\"" + dashWidth + "," + dashGap + "\" x1=\"" + svg.getX1() + "\" x2=\"" + svg.getX2() + "\" y1=\"" + svg.getY1() + "\" y2=\"" + svg.getY2() + "\" " +
+                        "stroke=\"" + svg.getColor() + "\" stroke-width=\"" + svg.getWidth() + "\"/>\n";
     }
-    private static String putText(SVGSingleShape svgShape) {
-        int x = svgShape.getX() + 10;
-        int y = svgShape.getY() + 35;
-        return "      <text x=\"" + x + "\" xml:space=\"preserve\" y=\"" + y + "\" clip-path=\"url(#clipPath20)\" stroke=\"none\"\n" +
-                "      font-family=\"" + svgShape.getFont() + "\" fill=\"#FFFFFF\"  >" + svgShape.getName() + "</text>\n";
+
+    private static String makeLineWithDoubleOrb(String line, ConnectionSVG svg) {
+
+        return " <circle cx=\"" + svg.getX1() + "\" cy=\"" + svg.getY1() + "\" r=\"3\" fill=\"" + svg.getColor() + "\" />" +
+                line +
+                "<circle cx=\"" + svg.getX2() + "\" cy=\"" + svg.getY2() + "\" r=\"3\" fill=\"" + svg.getColor() + "\" />";
+    }
+
+    private static String makeSingleOrb(ConnectionSVG svg, int x, int y) {
+
+        return " <circle cx=\"" + x + "\" cy=\"" + y + "\" r=\"3\" fill=\"" + svg.getColor() + "\" />";
+
+    }
+
+    private static String putText(ConnectionSVG svg, SVGSingleShape target) {
+
+        return (target.getConnectionsType()!=null?"<text x=\"" + (svg.getX1() + svg.getX2()) / 2 + "\" xml:space=\"preserve\" y=\"" + (svg.getY1() + svg.getY2()) / 2 + "\" clip-path=\"url(#clipPath20)\" stroke=\"none\"\n" +
+                "      font-family=\"" + target.getFont() + "\" fill=\"#000000\"  >" + target.getConnectionsType() + "</text>\n":"");
+//        return "      <text x=\"" + x + "\" xml:space=\"preserve\" y=\"" + y + "\" clip-path=\"url(#clipPath20)\" stroke=\"none\"\n" +
+//                "      font-family=\"" + svgShape.getFont() + "\" fill=\"#FFFFFF\"  >" + svgShape.getName() + "</text>\n";
     }
 }
